5e1ae9ca18d9b1f6bf4aec086004bbb04b9dcd50
alekanic <koirallabublik@gmail.com>
2023-05-21 15:40:17 +0300

    Second commit

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -2,28 +2,23 @@ package ru.netology.stats;
 
 public class StatsService {
 
-    public int sumOfSales(int[] sales) {
+    public int sumOfSales(int[] sales) {  // сумма всех продаж
         int sum = 0;
 
-        for (int i = 0; i < sales.length; i++) {
-            sum = sum + sales[i];
-            }
+        for (int sale : sales) {
+            sum += sale;
+        }
 
         return sum;
     }
 
-    public int averageSales(int[] sales) {
-        int sum = 0;
-
-        for (int i = 0; i < sales.length; i++) {
-            sum = sum + sales[i];
-        }
-
-        int averageSales = sum / sales.length;
+    public int averageSales(int[] sales) { // средняя сумма продаж за месяц
+        int averageSales = sumOfSales(sales) / sales.length;
         return averageSales;
+
     }
 
-    public int maxSales(long[] sales) {
+    public int maxSales(long[] sales) { // номер месяца с пиком продаж
         int maxMonth = 0;
         for (int i = 0; i < sales.length; i++) {
             if (sales[i] >= sales[maxMonth]) {
@@ -35,7 +30,7 @@ public class StatsService {
     }
 
 
-    public int minSales(long[] sales) {
+    public int minSales(long[] sales) { // номер месяца с дном продаж
         int minMonth = 0;
 
         for (int i = 0; i < sales.length; i++) {
@@ -47,36 +42,24 @@ public class StatsService {
         return minMonth + 1;
     }
 
-    public int belowTheAverage(int[] sales) {
+    public int belowTheAverage(int[] sales) { // количеством месяцев, где продажи ниже среднего
         int sum = 0;
-
-        for (int i = 0; i < sales.length; i++) {
-            sum = sum + sales[i];
-        }
-
-        int middleSales = sum / sales.length;
 
         int amountOfDays = 0;
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] < middleSales) {
+            if (sales[i] < averageSales(sales)) {
                 amountOfDays++;
             }
         }
         return amountOfDays;
     }
 
-    public int aboveTheAverage(int[] sales) {
+    public int aboveTheAverage(int[] sales) { // количество месяцев, где продажи выше среднего
         int sum = 0;
-
-        for (int i = 0; i < sales.length; i++) {
-            sum = sum + sales[i];
-        }
-
-        int middleSales = sum / sales.length;
 
         int amountOfDays = 0;
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] > middleSales) {
+            if (sales[i] > averageSales(sales)) {
                 amountOfDays++;
             }
         }
